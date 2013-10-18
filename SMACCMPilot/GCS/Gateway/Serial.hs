f9436ca5d9f1a3f91d5cc46a537ca8678440d16b
Pat Hickey <pat@galois.com>
2013-10-17 19:02:43 -0700

    fix to serial port behavior

--- SMACCMPilot/GCS/Gateway/Serial.hs
+++ SMACCMPilot/GCS/Gateway/Serial.hs
@@ -8,12 +8,10 @@ import qualified Data.ByteString as B
 import           Data.ByteString (ByteString)
 
 import           Control.Concurrent.Async
-import           Control.Exception
 import           Control.Monad
 
 import           Pipes
 import           Pipes.Concurrent
-import           System.IO
 import qualified System.Hardware.Serialport as SP
 
 import SMACCMPilot.GCS.Gateway.Opts
@@ -21,33 +19,26 @@ import SMACCMPilot.GCS.Gateway.Console
 import SMACCMPilot.GCS.Gateway.Async
 
 serialServer :: Options -> Console -> Output Word8 -> Input ByteString  -> IO ()
-serialServer opts console toser fromser = do
-  sp <- SP.hOpenSerial port settings
-  void $ forkIO $ server sp console toser fromser
+serialServer opts console fromser toser = void $ forkIO $ do
+  consoleLog console "Connected to serial client"
+  SP.withSerial port settings $ \serialport -> do
+    a1 <- asyncEffect "serial input" $
+            serialInput serialport >-> toOutput fromser
+    a2 <- asyncEffect "serial output" $
+            fromInput toser >-> serialOutput serialport
+    mapM_ wait [a1, a2]
+  consoleLog console "Closed serial client"
   where
   settings = SP.defaultSerialSettings { SP.commSpeed = serBaud opts }
   port = serPort opts
 
-server :: Handle -> Console -> Output Word8 -> Input ByteString -> IO ()
-server serialport console outp inp = do
-  consoleLog console "Connected to serial client"
-  hSetBuffering serialport NoBuffering
-  a1 <- asyncEffect "serial input" $
-          serialInput serialport >-> toOutput outp
-  a2 <- asyncEffect "serial output" $
-          fromInput inp >-> serialOutput serialport
-  finally (mapM_ wait [a1, a2])
-          (hClose serialport)
-  consoleLog console "Closed serial client"
-
-serialInput :: Handle -> Producer Word8 IO r
-serialInput h = forever $ do
-  bs <- lift $ B.hGet h 16
+serialInput :: SP.SerialPort -> Producer Word8 IO r
+serialInput sp = forever $ do
+  bs <- lift $ SP.recv sp 16
   mapM_ yield (B.unpack bs)
 
-serialOutput :: Handle -> Consumer ByteString IO r
-serialOutput h = do
+serialOutput :: SP.SerialPort -> Consumer ByteString IO r
+serialOutput sp = forever $ do
   b <- await
-  _ <- lift $ B.hPut h b
-  serialOutput h
+  void $ lift $ SP.send sp b
 
